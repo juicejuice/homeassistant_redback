0d0d3ac944928e450b66132d07644e61ef3d6d49
juicejuice <juicejuice@gmail.com>
2022-09-11 01:47:12 -0400

    Added test mode to Redback library

--- custom_components/redback/redbacklib.py
+++ custom_components/redback/redbacklib.py
@@ -21,6 +21,7 @@ class RedbackInverter:
     _energyData = None
     _energyDataUpdateInterval = timedelta(minutes=1)
     _energyDataNextUpdate = datetime.now()
+    _testMode = False
 
     def __init__(self, cookie, serial):
         """ Constructor: needs API auth cookie and inverter serial number """
@@ -31,6 +32,10 @@ class RedbackInverter:
 
     def _apiRequest(self, apimethod):
         """ Call into Redback cloud API """
+
+        # return test data instead of real API calls
+        if (self._testMode):
+            return self._apiTestRequest(apimethod)
 
         if (apimethod == 'energyflowd2'):
             # https://portal.redbacktech.com/api/v2/energyflowd2/$SERIAL
@@ -56,6 +61,20 @@ class RedbackInverter:
             raise RedbackError(f'JSON Error. {e.msg}. Pos={e.pos} Line={e.lineno} Col={e.colno}')
 
         return data
+
+    def _apiTestRequest(self, apimethod):
+        match apimethod:
+            case 'inverterinfo':
+                return {'Model': 'ST10000', 'Firmware': '080819', 'RossVersion': '2.15.32207.13', 'IsThreePhaseInverter': True, 'IsSmartBatteryInverter': False, 'IsSinglePhaseInverter': False, 'IsGridTieInverter': False}
+            case 'BannerInfo':
+                return {'ProductDisplayname': 'Smart Inverter TEST', 'InstalledPvSizeWatts': 9960.0, 'BatteryCapacityWattHours': 14000.001}
+            case 'energyflowd2':
+                return {'Data':{'Input':{'ACLoadW': 123.0, 'BackupLoadW': 0.0, 'SupportsConnectedPV': True, 'PVW': 456.0, 'ThirdPartyW': None, 'GridStatus': 'Export', 'GridNegativeIsImportW': -123.0, 'ConfiguredWithBatteries': True, 'BatteryNegativeIsChargingW': 403.308, 'BatteryStatus': 'Discharging', 'BatterySoC0to100': 51.0, 'CtComms': True}}}
+                
+        raise RedbackError('Test Mode: unknown API method %s', apimethod)
+
+    def setTestMode(self, testMode):
+        self._testMode = testMode
 
     def getInverterInfo(self):
         """ Returns inverter info (static data, updated first use only) """
