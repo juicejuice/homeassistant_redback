4617028aecff445611a126ec88c8d5a8e0d75e6d
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2023-07-11 11:45:36 +1000

    Update sensor.py

--- custom_components/redback/sensor.py
+++ custom_components/redback/sensor.py
@@ -437,7 +437,7 @@ class RedbackChargeSensor(RedbackEntity, SensorEntity):
     _attr_has_entity_name = True
     _attr_state_class = SensorStateClass.MEASUREMENT
     _attr_native_unit_of_measurement = PERCENTAGE
-    _attr_device_class = SensorDeviceClass.BATTERY
+    _attr_device_class = SensorDeviceClass.ENERGY
     
     @property
     def unique_id(self) -> str:
